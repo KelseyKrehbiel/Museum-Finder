ca5604e8e4d285ae072809ab5f02706b3728b12f
Klordok <blargsmarg@gmail.com>
2020-11-18 00:12:15 -0700

    display list of places

--- script.js
+++ script.js
@@ -2,51 +2,50 @@
 
 const myKey = '5ae2e3f221c38a28845f05b6dab77e254cd36150a97eb1ef1812beef'
 
-function buildAddressRequest(stateList,results){
-    //build request URL for getting facility addresses
-    console.log("building request");
-    let requestURL = "https://ridb.recreation.gov/api/v1/facilityaddresses?";
 
-    const options = {
-        mode: 'cors',
-        headers: {
-          "Accept": "application/json",
-          "apiKey": myKey
-        }
-        
-    };
-    let encodedStateList = stateList.replace(/\s/g, "");
-    //convert commas to %2C for URL
-    encodedStateList = encodedStateList.replace(',','%2C');
-    //console.log(encodedStateList);
-    requestURL += `query=${encodedStateList}&limit=${results}`;
-
-    //console.log(requestURL);
-    apiGet(options)
-}
-
-function apiGet(query) {
+function getCoordinates(stateList) {
     return new Promise(function(resolve, reject) {
       var otmAPI =
         "https://api.opentripmap.com/0.1/en/places/" +
         'geoname' +
         "?apikey=" +
-        myKey+ "&name=arizona";
+        myKey+ `&name=${stateList}`;
 
       fetch(otmAPI)
         .then(response => response.json())
-        .then(data => resolve(data))
+        .then(data => {
+          resolve(data)
+          let coordinates = {"lat": data.lat, "lon":data.lon}
+          console.log(data);
+          getPlaces(coordinates);
+        })
         .catch(function(err) {
           console.log("Fetch Error :-S", err);
         });
+        console.log("This is the json");
+        
+
     });
   }
 
-  function requestFacilityData(){
-      //use address data to build fetch request for facilites
-      //"https://ridb.recreation.gov/api/v1/facilities/{facilityID}"
-      //for each facility request data
-      //get Name, Description, address, longitude, latitude
+  function getPlaces(coordinates){
+    //use lat and lon to get list of museums in area
+    let placesURL = "https://api.opentripmap.com/0.1/en/places/radius?radius=30000"+
+      `&lon=${coordinates.lon}&lat=${coordinates.lat}`+
+      "&kinds=museums"+
+      "&format=json"+
+      `&apikey=${myKey}`;
+
+    fetch(placesURL)
+    .then(response => response.json())
+    .then(data => {
+      //resolve(data)
+      console.log(data)
+      displayResults(data);
+    })
+    .catch(function(err) {
+      console.log("Fetch Error :-S", err);
+    });
   }
 
   function getFacilityMap(){
@@ -56,16 +55,24 @@ function apiGet(query) {
 
   //
   
-  function displayResults() {
-    console.log();
+  function displayResults(data) {
+    console.log("display results");
     //Clear the previous results
-
-    //For each facility make a list item
-    //Facility Name
+    $('body').remove('ul','li');
+    //For each place make a list item
+    //place Name
     //Description
     //Address
     //Website
     //Google Map
+    $('main').append('<ul class="place-list"></ul>')
+    data.forEach(element => {
+      let itemName = element.name;
+      if (itemName != ""){
+        $('ul').append(`<li>${itemName}</li>`);
+      }     
+        
+    })
 
     //Add it to the DOM
 
@@ -80,8 +87,22 @@ function apiGet(query) {
       //console.log($('#result-size').val());
       let stateList = $('#state-list').val();
       let results = $('#result-size').val();
-      buildAddressRequest(stateList,results);
+      getCoordinates(stateList);
     });
   }
   //wait for submission
   $(watchForm());
+
+
+
+
+  /*
+let xid = element.xid;
+      let placeURL = `https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=${myKey}`
+      console.log(placeURL);
+      fetch(placeURL)
+      .then(response => response.json())
+      .then(pdata => {
+        //resolve(data)
+        console.log(pdata)
+  */
