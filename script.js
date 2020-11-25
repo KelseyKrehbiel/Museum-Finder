a6cb0c2738bdd06ba7a87896e85144ef13316a09
Klordok <blargsmarg@gmail.com>
2020-11-25 00:58:19 -0700

    functional site with style

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-import { google } from "https://maps.googleapis.com/maps/api/js?key=AIzaSyB1tEvB-iDEeyFXyjp2FC122VPDzXrWqcE&callback=initMap&libraries=&v=weekly";
+//import { google } from "https://maps.googleapis.com/maps/api/js?key=AIzaSyB1tEvB-iDEeyFXyjp2FC122VPDzXrWqcE&callback=initMap&libraries=&v=weekly";
 "use strict";
 
 const otmKey = '5ae2e3f221c38a28845f05b6dab77e254cd36150a97eb1ef1812beef'
 
+let offset = 0;
 
-function getCoordinates(location,radius) {
+let coordinates = {"lat":0, "lon":0};
+
+let radius = 3000;
+
+const pageLength = 5
+
+function getCoordinates(location) {
     return new Promise(function(resolve, reject) {
       var otmAPI =
         "https://api.opentripmap.com/0.1/en/places/" +
@@ -16,7 +23,7 @@ function getCoordinates(location,radius) {
         .then(response => response.json())
         .then(data => {
           resolve(data)
-          let coordinates = {"lat": data.lat, "lon":data.lon}
+          coordinates = {"lat": data.lat, "lon":data.lon}
           console.log(data);
           getPlaces(coordinates,radius);
         })
@@ -29,18 +36,17 @@ function getCoordinates(location,radius) {
     });
   }
 
-  function getPlaces(coordinates,radius){
+  function getPlaces(){
     //use lat and lon to get list of museums in area
     let placesURL = "https://api.opentripmap.com/0.1/en/places/radius?"+
       `radius=${radius}`+
-      "&limit=15"+
+      `&limit=${pageLength}`+
+      `&offset=${offset}`+
       `&lon=${coordinates.lon}&lat=${coordinates.lat}`+
       "&kinds=museums"+
       "&format=json"+
       `&apikey=${otmKey}`;
-    console.log(placesURL);
-    
-    //getPlaceMap(coordinates); 
+    console.log(placesURL); 
 
     fetch(placesURL)
     .then(response => response.json())
@@ -54,15 +60,15 @@ function getCoordinates(location,radius) {
     });
   }
 
-  function getPlaceMap(coordinates){
+  function getPlaceMap(coordinates,xid){
       //get map location of facility based on longitude and latitude
       //use Google maps
       console.log("getting map");
       let gCoords = {'lat': coordinates.lat, 'lng': coordinates.lon}
 
       // The map, centered at gCoords
-      const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+      const map = new google.maps.Map(document.getElementById(xid), {
+        zoom: 17,
         center: gCoords,
       });
       // The marker, positioned at gCoords
@@ -70,7 +76,6 @@ function getCoordinates(location,radius) {
         position: gCoords,
         map: map,
       });
-
 
   }
 
@@ -80,13 +85,11 @@ function getCoordinates(location,radius) {
     console.log("display results");
     //Clear the previous results
     $(".place-list").remove();
+    $("no-result").remove();
+    $("#next-button").remove();
     //For each place make a list item
-    //place Name
-    //Description
-    //Address
-    //Website
-    //Google Map
-    $('main').append('<ol class="place-list"></ol>')
+    //Description,Google Map
+    $('main').append('<ul class="place-list"></ul>')
     data.forEach(element => {
       let itemName = element.name;
       if (itemName != ""){
@@ -94,7 +97,6 @@ function getCoordinates(location,radius) {
         let placeURL = `https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=${otmKey}`
         //console.log(placeURL);
         //delay calls to stay in api limit
-        
         fetch(placeURL)
         .then(response => response.json())
         .then(pdata => {
@@ -105,29 +107,33 @@ function getCoordinates(location,radius) {
           let address = `${pdata.address.city}, ${pdata.address.state}`
           
           //get wikipedia description from place data
-          let wikiExtract = pdata.wikipedia_extracts.text;
+          let wikiExtract = pdata.wikipedia_extracts ? pdata.wikipedia_extracts.text : "No info available.";
         
-        //add items to list
-        $('ol').append(`<li>
-                          <h2>${itemName}</h2>
-                          <h3>${address}</h3>
-                          <p>${wikiExtract}</p>
-                          <div id="map">map goes here</div>
-                        </li>`);
+          //add items to list
+          $('.place-list').append(`<li>
+                            <h2 class="center">${itemName}</h2>
+                            <fieldset class="place-container">
+                              <div class="place-data"><p>${wikiExtract}</p></div>
+                              <div class="place-data" id=${xid}></div>
+                            </fieldset>
+                          </li>`);
+
+          //get map for location
+          getPlaceMap(pdata.point,xid);
         })
-        .then(sleeper(1000));
+        
       } 
     });
-
+    //check if more results available
+    if(data.length < 5){
+      $('main').append(`<h2 class="no-result center">End of results</h2>`);
+    }
+    else{
+      //button to load next set of results
+      $('main').append(`<button id="next-button" name="next-button" class="center">Load More</button>`);
+    }
     //Add it to the DOM
 
-
-  }
-
-  function sleeper(ms) {
-    return function(x) {
-      return new Promise(resolve => setTimeout(() => resolve(x), ms));
-    };
   }
   
   function watchForm() {
@@ -138,9 +144,19 @@ function getCoordinates(location,radius) {
       //console.log($('#search-radius').val());
       let location = $('#location').val();
       //convert kilometers to meters
-      let radius = ($('#search-radius').val())*1000;
+      radius = ($('#search-radius').val())*1000;
+      //reset offset to 0
+      offset = 0;
       getCoordinates(location,radius);
     });
+
+    $('main').on('click', '#next-button', event => {
+      console.log("next")
+      event.preventDefault();
+      offset += 5;
+      getPlaces();
+    });
+
   }
   //wait for submission
   $(watchForm());
